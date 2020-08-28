6947b9c337149fda597222bfb2d31f215e422b84
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-08-28 17:12:48 -0400

    basic setup, sass, router

---
+++ src/components/Board.jsx
@@ -0,0 +1,8 @@
+
+import React, { useState } from "react";
+
+function Board() {
+  return <h1>Board</h1>
+}
+
+export default Board;
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function Home() {
+    return <h1>Home</h1>
+  }
+
+
+export default Home;
