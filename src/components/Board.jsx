3db9054da7d0255d58cd5adc2c2cf74ce968c901
Cheriele Mordido <eyelmordido@gmail.com>
2020-08-28 18:00:32 -0400

    rendered fake array on page

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,52 @@
-
 import React, { useState } from "react";
+import Ticket from './Ticket';
 
 function Board() {
-  return <h1>Board</h1>
+  const [columns, setColumns] = useState([
+    {
+      title: 'To Do',
+      tickets: [
+        {title: 'create component',
+          details: 'write code for board component.'}]
+    },
+    {
+      title: 'In Progress',
+      tickets: [
+        {title: 'create component',
+          details: 'write code for board component.'}]
+    },
+    {
+      title: 'Done',
+      tickets: [
+        {title: 'create component',
+          details: 'write code for board component.'}]
+    }
+  ]);
+
+  return (
+    <>
+    {
+      columns.map((column, index) => {
+        return (
+          <div key={`${column.title}${index}`}>
+            <h2>{column.title}</h2>
+            <ul>
+              {
+                column.tickets.map((ticket, index) => {
+                  return(
+                    <li key={`${ticket.title}${index}`}>
+                      <Ticket title={ticket.title} details={ticket.details}/>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        )
+      })
+    }
+    </>
+  )
 }
 
 export default Board;
