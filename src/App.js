c009b5e6b573e11811b20c20be8e2845776b2a0a
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-28 15:48:39 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import './styles/index.css';
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       hello
     </div>
   );
 }
