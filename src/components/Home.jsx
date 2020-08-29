0b8001bc53ac86c1edcfdec143ae79b968ad9513
Cheriele Mordido <eyelmordido@gmail.com>
2020-08-28 21:14:12 -0400

    added variables and extra font weights

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
+import headerImg from '../assets/undraw_online_organizer_ofxm.svg';
 
 function Home() {
-    return <h1>Home</h1>
+    return (
+      <>
+        <section className='wrapper intro'>
+          <div className='introText'>
+            <h1>Maximize Productivity</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eum non hic dolore praesentium optio fuga quasi nostrum sapiente recusandae quas, voluptatibus et cumque iusto quibusdam error perspiciatis libero.</p>
+          </div>
+          <div className='introImg'>
+            <img src={headerImg}/>
+          </div>
+        </section>
+      </>
+    )
   }
 
 
