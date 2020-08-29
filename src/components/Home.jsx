09f4d6faeea78e07b6505e0f8eddd28191039a8f
Cheriele Mordido <eyelmordido@gmail.com>
2020-08-28 22:12:02 -0400

    added founders section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import headerImg from '../assets/undraw_online_organizer_ofxm.svg';
+import {
+  FaLinkedinIn,
+  FaExternalLinkAlt,
+  FaTwitter,
+} from "react-icons/fa";
 
 function Home() {
     return (
-      <>
-        <section className='wrapper intro'>
+      <div className='wrapper'>
+        <section className='intro'>
           <div className='introText'>
             <h1>Maximize Productivity</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eum non hic dolore praesentium optio fuga quasi nostrum sapiente recusandae quas, voluptatibus et cumque iusto quibusdam error perspiciatis libero.</p>
@@ -13,7 +18,43 @@ function Home() {
             <img src={headerImg}/>
           </div>
         </section>
-      </>
+        
+        <section className='founders'>
+          <h2>Founded by</h2>
+          <ul className='foundersContainer'>
+            <li className='foundersCard'>
+              <img src='https://placeimg.com/400/400/people' />
+              <h3>Lorem, ipsum.</h3>
+              <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, iste!</p>
+              <ul className='foundersSns'>
+                <li><a href='#'><FaLinkedinIn/></a></li>
+                <li><a href='#'><FaTwitter/></a></li>
+                <li><a href='#'><FaExternalLinkAlt/></a></li>
+              </ul>
+            </li>
+            <li className='foundersCard'>
+              <img src='https://placeimg.com/400/400/people' />
+              <h3>Lorem, ipsum.</h3>
+              <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, iste!</p>
+              <ul className='foundersSns'>
+                <li><a href='#'><FaLinkedinIn/></a></li>
+                <li><a href='#'><FaTwitter/></a></li>
+                <li><a href='#'><FaExternalLinkAlt/></a></li>
+              </ul>
+            </li>
+            <li className='foundersCard'>
+              <img src='https://placeimg.com/400/400/people' />
+              <h3>Lorem, ipsum.</h3>
+              <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, iste!</p>
+              <ul className='foundersSns'>
+                <li><a href='#'><FaLinkedinIn/></a></li>
+                <li><a href='#'><FaTwitter/></a></li>
+                <li><a href='#'><FaExternalLinkAlt/></a></li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </div>
     )
   }
 
