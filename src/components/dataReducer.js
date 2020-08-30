2a57694a5e5a77fc1d65cb80072bb45b65b5f2fd
oksana sam <oksana.o.samokhvalova@gmail.com>
2020-08-30 00:23:14 -0400

    merged boards

--- src/components/dataReducer.js
+++ src/components/dataReducer.js
@@ -39,6 +39,11 @@ function dataReducer(state, action){
                 }
             }
         };
+        // case "UPDATE_COLUMN_ORDER": 
+        // return {
+        //     ...state,
+        //     columnOrder: action.payload
+        // };
           
         default:
             return state;
@@ -47,3 +52,60 @@ function dataReducer(state, action){
 }
 
 export default    dataReducer;
+
+
+
+
+
+
+
+
+
+// "ADD_COLUMN"
+// const addedColumns = {
+//     ...state,
+//     columnOrder: [...state.columnOrder, action.payload],
+//     columns: {...state.columns,
+//     [uuid()]: {id: action.payload, title: '', ticketIds: []} }
+// }
+
+// setState(addedColumns)
+
+
+
+// "DELETE_COLUMN"
+// const { [column.id]: value, ...rest } = state.columns;
+// const columnsAfterDelete = {
+//     ...state,
+//     columnOrder: state.columnOrder.filter(item => item !== column.id),
+//     columns: {...rest}
+// }
+
+// setState(columnsAfterDelete)
+
+
+
+
+// "ADD_TASK"
+// const randomTicketId = uuid();
+// const addedTasks = {
+//     ...state,
+
+//     tickets: {
+//         ...state.tickets, 
+//         [randomTicketId]: {
+//             id: randomTicketId, 
+//             title: '', 
+//             details: '', 
+//         },
+//     },
+//     columns: {
+//         ...state.columns,
+//         [column.id]: {
+//             ...state.columns[column.id],
+//             ticketIds: [...state.columns[column.id].ticketIds, randomTicketId]
+//         }
+//     }
+// }
+
+// setState(addedTasks)
