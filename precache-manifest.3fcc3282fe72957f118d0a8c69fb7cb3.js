bf26ef0344b1dd2fa44c58b50f46fb4fc7aa9ff1
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-30 21:58:25 -0400

    Updates

---
+++ precache-manifest.3fcc3282fe72957f118d0a8c69fb7cb3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a5cb5784e2ddc3a436d78d685fa67c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63cbcf59df2bca0760d1",
+    "url": "./static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "e4f26a6e649aa9bd5b2a",
+    "url": "./static/css/main.d46299be.chunk.css"
+  },
+  {
+    "revision": "63cbcf59df2bca0760d1",
+    "url": "./static/js/2.c3e270cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c3e270cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f26a6e649aa9bd5b2a",
+    "url": "./static/js/main.000d3cf7.chunk.js"
+  },
+  {
+    "revision": "92e533f610961626a002",
+    "url": "./static/js/runtime-main.893301b1.js"
+  },
+  {
+    "revision": "c6ef4e9ede09d52d5920215844740afc",
+    "url": "./static/media/juno-logo.c6ef4e9e.png"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "./static/media/mintbean.cf18b629.png"
+  },
+  {
+    "revision": "8f65a03390d620df8a81faebb72bffb4",
+    "url": "./static/media/reactIcon.8f65a033.png"
+  },
+  {
+    "revision": "e846e8b4dc00660dc78e60d913815c00",
+    "url": "./static/media/undraw_Eiffel_tower_3gw8.e846e8b4.png"
+  },
+  {
+    "revision": "5570c20721fd12c4634593de15b38d79",
+    "url": "./static/media/undraw_dev_productivity_umsq.5570c207.svg"
+  },
+  {
+    "revision": "d7c62bef1288d44ec1e315f4760f3b72",
+    "url": "./static/media/undraw_online_organizer_ofxm.d7c62bef.svg"
+  },
+  {
+    "revision": "ff5566a0a150a165c7555f5f8028a80f",
+    "url": "./static/media/undraw_subscriber_vabu.ff5566a0.svg"
+  }
+]);
